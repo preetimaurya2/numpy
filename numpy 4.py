74cd0c4d59c1f20109900e166fd31ad032e4fac4
preetimaurya2 <97082407+preetimaurya2@users.noreply.github.com>
2022-03-28 23:49:54 +0530

    Add files via upload

---
+++ numpy 4.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+4. Create a null vector of size 10 but the fifth value which is 1
+
+
+# In[1]:
+
+
+import numpy as np
+a=np.zeros(10)
+a[4]=1
+print(a)
+
