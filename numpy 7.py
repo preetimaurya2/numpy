1a8be7d04e3c781449241246a8f22369ee26d8c9
preetimaurya2 <97082407+preetimaurya2@users.noreply.github.com>
2022-04-03 19:18:18 +0530

    Add files via upload

---
+++ numpy 7.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+7. Create a 3x3 matrix with values ranging from 0 to 8.
+
+
+# In[1]:
+
+
+import numpy as np
+a=np.arange(9).reshape(3,3)
+print(a)
+
