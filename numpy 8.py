f4fcf91e6cb455e9c85a77868b11dddb3f15acd8
preetimaurya2 <97082407+preetimaurya2@users.noreply.github.com>
2022-04-04 22:51:37 +0530

    Add files via upload

---
+++ numpy 8.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+8. Find indices of non-zero elements from [1,2,0,0,4,0]
+
+
+# In[11]:
+
+
+import numpy as np
+a=np.array([1,2,0,0,4,0])
+np.nonzero(a)
+
