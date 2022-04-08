490596c13a3a88915078ed28864a24d7124ae602
preetimaurya2 <97082407+preetimaurya2@users.noreply.github.com>
2022-04-09 00:04:23 +0530

    Add files via upload

---
+++ Numpy 11.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+11. Create a 10x10 array with random values and find the minimum and maximum values.
+
+
+# In[2]:
+
+
+import numpy as np
+a=np.random.random((10,10))
+print(a)
+print('max',a.max())
+print('min',a.min())
+
+
+# In[ ]:
+
+
+
+
