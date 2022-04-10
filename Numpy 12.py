37b7879c8eb7bd3597169494cac4edfaf2942809
preetimaurya2 <97082407+preetimaurya2@users.noreply.github.com>
2022-04-10 21:16:49 +0530

    Add files via upload

---
+++ Numpy 12.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+12. Create a random vector of size 30 and find the mean value
+
+
+# In[ ]:
+
+
+import numpy as np
+a=np.random.random(30)
+print(a)
+print('\n')
+print('mean:',round(a.mean(),2))
+
