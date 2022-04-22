71d44088ff2dbf5411f9d2c5355596e6c28c1fb2
preetimaurya2 <97082407+preetimaurya2@users.noreply.github.com>
2022-04-22 22:19:19 +0530

    Add files via upload

---
+++ Numpy 21.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+21. Multiply a 5x3 matrix by a 3x2 matrix (real matrix product)  
+
+
+# In[4]:
+
+
+import numpy as np
+a=np.ones((5,3))
+b=np.ones((3,2))
+c=np.dot(a,b)
+print(c)
+
+
+# In[ ]:
+
+
+
+
