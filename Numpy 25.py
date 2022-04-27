cd0eab6ae6c01bd167962a5e08eb2839c4af7671
preetimaurya2 <97082407+preetimaurya2@users.noreply.github.com>
2022-04-27 23:09:57 +0530

    Add files via upload

---
+++ Numpy 25.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+25.Is the following expressions true? 
+np.sqrt(-1) == np.emath.sqrt(-1)
+
+
+# In[2]:
+
+
+import numpy as np
+
+np.sqrt(-1)==np.emath.sqrt(-1)
+
