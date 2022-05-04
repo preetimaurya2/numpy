1df7fff0d4aa1a04e285c0626a94cdb42e61c885
preetimaurya2 <97082407+preetimaurya2@users.noreply.github.com>
2022-05-04 23:55:30 +0530

    Add files via upload

---
+++ Numpy 29.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+29. Create a 5x5 matrix with row values ranging from 0 to 4 
+
+
+# In[8]:
+
+
+import numpy as np
+a=np.random.randint(0,5,(5,5))
+print(a)
+
