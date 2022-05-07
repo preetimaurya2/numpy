a01fabb803087e596c57c90df06a701c76394080
preetimaurya2 <97082407+preetimaurya2@users.noreply.github.com>
2022-05-07 23:36:34 +0530

    Add files via upload

---
+++ Numpy 32.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+32. Create a random vector of size 10 and sort it
+
+
+# In[1]:
+
+
+import numpy as np
+a=np.random.random(10)
+b=np.sort(a)
+print(b)
+
